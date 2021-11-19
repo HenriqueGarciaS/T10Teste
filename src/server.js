ca85cf2c5600009af2a5a2bcc599f0bfc1b4a39c
Henrique Silva <henrique9811@hotmail.com>
2021-11-19 09:44:45 -0300

    fix cors

--- src/server.js
+++ src/server.js
@@ -3,11 +3,15 @@ const routes = require('./routes');
 require('./database');
 const app = express();
 const cors = require('cors');
+const corsOptions = {
+    origin : "*",
+    optionsSucessStatus:200
+}
 
-
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 5000);
 
-console.log("Escutando na porta 3000");
+console.log("Escutando na porta 5000");
